e2076c971a5a6898aeb16019c9ce8c595627a568
Ivan Puzankov <ivan.puzankov@akbarsdigital.ru>
2017-12-02 20:50:29 +0300

    Base template for one product and rndering of it with fake date

--- shop/server.py
+++ shop/server.py
@@ -20,6 +20,17 @@ def root():
     return render_template('products_list.html', products=products)
 
 
+@app.route('/<product_id>')
+def product(product_id):
+    product = {
+        'name': 'one',
+        'picture_link': 'https://assets.pcmag.com/media/images/396774-the-10-best-ultraportables.jpg',
+        'description': 'this is the description of the product'
+    }
+
+    return render_template('product.html', product=product)
+
+
 if __name__ == '__main__':
     host = '0.0.0.0'
     port = 8080
